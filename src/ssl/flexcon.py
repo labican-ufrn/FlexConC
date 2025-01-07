a80e9c21c4b2e159b472e9be4073c600088a96b4
ArthurGorgonio <gorgonioarthur@gmail.com>
2025-01-07 00:21:00 -0300

    #15: Melhora qualidade de código e reverte algumas alterações

--- src/ssl/flexcon.py
+++ src/ssl/flexcon.py
@@ -1,5 +1,4 @@
 from abc import abstractmethod
-from typing import Dict, List, Optional
 
 import numpy as np
 from sklearn.base import clone
@@ -14,66 +13,99 @@ class BaseFlexConC(SelfTrainingClassifier):
         modelos de aprendizado semisupervisionado
     """
 
-    def __init__(self, base_estimator, cr=0.05, threshold=0.95, verbose=False):
+    def __init__(
+        self,
+        base_estimator,
+        cr: float = 0.05,
+        threshold: float = 0.95,
+        verbose: bool = False
+    ):
         super().__init__(
-            base_estimator=base_estimator, threshold=threshold, max_iter=100
+            base_estimator=base_estimator,
+            threshold=threshold,
+            max_iter=100
         )
         self.cr: float = cr
         self.threshold: float = threshold
-        self.verbose = verbose
-        self.old_selected: List = []
-        self.dict_first: Dict = {}
-        self.init_labeled_: List = []
-        self.labeled_iter_: List = []
-        self.transduction_: List = []
-        self.classes_: List = []
+        self.verbose: bool = verbose
+        self.old_selected: list = []
+        self.dict_first: dict = {}
+        self.init_labeled_: list = []
+        self.labeled_iter_: list = []
+        self.transduction_: list = []
+        self.classes_: list = []
         self.termination_condition_ = ""
-        self.pred_x_it: Dict = {}
-        self.cl_memory: List = []
+        self.pred_x_it: dict = {}
+        self.cl_memory: list = []
         self.base_estimator_ = clone(self.base_estimator)
         self.base_estimator_select_ = clone(self.base_estimator)
 
+    def __str__(self) -> str:
+        msg = super().__str__()
+        msg += (
+            f"Classificador {self.base_estimator}\n"
+            f"Outros Parâmetro:"
+            f" CR: {self.cr}\t Threshold: {self.threshold}"
+            f" Máximo IT: {self.max_iter}"
+        )
+
+        return msg
+
     @abstractmethod
     def fit(self, X, y):
-        ...
+        raise NotImplementedError('Implement me')
 
     def validate(self):
         """
-            Validate the fitted estimator since `predict_proba` can be
-            delegated to an underlying "final" fitted estimator as
-            generally done in meta-estimator or pipeline.
+        Validate the fitted estimator since `predict_proba` can be
+        delegated to an underlying "final" fitted estimator as
+        generally done in meta-estimator or pipeline.
         """
         try:
             if not hasattr(self.base_estimator, "predict_proba"):
                 msg = "base_estimator ({}) should implement predict_proba!"
-                raise ValueError(msg.format(type(self.base_estimator).__name__))
+                raise ValueError(
+                    msg.format(type(self.base_estimator).__name__)
+                )
         except ValueError:
             return False
+
         return True
 
-    def calc_local_measure(self, X, y_true, classifier):
-        """ge
-        Calcula o valor da acurácia do modelo
+    def calc_local_measure(
+        self,
+        X: list,
+        y_true: list,
+        classifier
+    ) -> float:
+        """
+        Calcula o valor da acurácia do modelo.
 
         Args:
-            X: instâncias
-            y_true: classes
-            classifier: modelo
+            - X: instâncias.
+            - y_true: classes.
+            - classifier: modelo.
 
         Returns:
-            Retorna a acurácia do modelo
+            Retorna a acurácia do modelo.
         """
         y_pred = classifier.predict(X)
+
         return accuracy_score(y_true, y_pred)
 
-    def new_threshold(self, local_measure, init_acc):
+    def new_threshold(
+        self,
+        local_measure: float,
+        init_acc: float
+    ) -> None:
         """
-        Responsável por calcular o novo limiar
+        Responsável por calcular o novo limiar.
 
         Args:
-            local_measure: valor da acurácia do modelo treinado
-            init_acc: valor da acurácia inicial
+            - local_measure: valor da acurácia do modelo treinado.
+            - init_acc: valor da acurácia inicial.
         """
+
         if local_measure > (init_acc + 0.01) and (
             (self.threshold - self.cr) > 0.0
         ):
@@ -82,59 +114,76 @@ class BaseFlexConC(SelfTrainingClassifier):
             (self.threshold + self.cr) <= 1
         ):
             self.threshold += self.cr
-        else:
-            pass
 
     def update_memory(
-        self, instances: List, labels: List, weights: Optional[List] = None
+        self,
+        instances: list,
+        labels: list,
+        weights: list | None = None,
     ) -> None:
         """
-        Atualiza a matriz de instâncias rotuladas
+        Atualiza a matriz de instâncias rotuladas.
 
         Args:
-            instances: instâncias
-            labels: rotulos
-            weights: Pesos de cada classe
+            - instances: instâncias.
+            - labels: rotulos.
+            - weights: Pesos de cada classe.
         """
+
         if not weights:
             weights = [1 for _ in range(len(instances))]
 
         for instance, label, weight in zip(instances, labels, weights):
             self.cl_memory[instance][label] += weight
 
-    def remember(self, X: List) -> List:
+    def remember(self, X: list) -> list:
         """
-        Responsável por armazenar como está as instâncias dado um  momento no
-        código
+        Responsável por armazenar como está as instâncias dado um
+        momento no código.
 
         Args:
-            X: Lista com as instâncias
+            - X: lista com as instâncias.
 
         Returns:
-            A lista memorizada em um dado momento
+            A lista memorizada em um dado momento.
         """
+
         return [np.argmax(self.cl_memory[x]) for x in X]
 
     def storage_predict(
-        self, idx, confidence, classes
-    ) -> Dict[int, Dict[float, int]]:
+        self,
+        idx,
+        confidence,
+        classes,
+    ) -> dict[int, dict[float, int]]:
         """
-        Responsável por armazenar o dicionário de dados da matriz
+        Responsável por armazenar o dicionário de dados da matriz.
 
         Args:
-            idx: indices de cada instância
-            confidence: taxa de confiança para a classe destinada
-            classes: indices das classes
+            - idx: indices de cada instância.
+            - confidence: taxa de confiança para a classe destinada.
+            - classes: indices das classes.
 
         Returns:
-            Retorna o dicionário com as classes das instâncias não rotuladas
+            Retorna o dicionário com as classes das instâncias não
+            rotuladas.
+
+            .. code-block: python
+            memo = {
+                <idx>: {
+                    'confidence': 0.9,
+                    'classes': 3
+                }
+                # ... outras instancias
+            }
+
         """
         memo = {}
 
-        for i, conf, label in zip(idx, confidence, classes):
-            memo[i] = {}
-            memo[i]["confidence"] = conf
-            memo[i]["classes"] = label
+        for instance, conf, label in zip(idx, confidence, classes):
+            memo[instance] = {}
+            memo[instance]["confidence"] = conf
+            memo[instance]["classes"] = label
 
         return memo
 
@@ -144,20 +193,21 @@ class BaseFlexConC(SelfTrainingClassifier):
             confianças preditas é maior que o limiar
 
         Returns:
-            a lista correspondente pela condição
+            - A lista correspondente pela condição;
+            - A classe de cada elemento da lista.
         """
         selected = []
         classes_selected = []
 
-        for i in self.pred_x_it:
+        for instance, values in self.pred_x_it.items():
             if (
-                self.dict_first[i]["confidence"] >= self.threshold
-                and self.pred_x_it[i]["confidence"] >= self.threshold
-                and self.dict_first[i]["classes"]
-                == self.pred_x_it[i]["classes"]
+                self.dict_first[instance]["confidence"] >= self.threshold
+                and values["confidence"] >= self.threshold
+                and self.dict_first[instance]["classes"]
+                == values["classes"]
             ):
-                selected.append(i)
-                classes_selected.append(self.dict_first[i]["classes"])
+                selected.append(instance)
+                classes_selected.append(self.dict_first[instance]["classes"])
 
         return selected, classes_selected
 
@@ -167,62 +217,64 @@ class BaseFlexConC(SelfTrainingClassifier):
         confianças preditas é maior que o limiar
 
         Returns:
-        a lista correspondente pela condição
+            - A lista correspondente pela condição;
+            - A classe de cada elemento da lista.
         """
 
         selected = []
         classes_selected = []
 
-        for i in self.pred_x_it:
+        for instance, values in self.pred_x_it.items():
             if (
-                self.dict_first[i]["confidence"] >= self.threshold
-                or self.pred_x_it[i]["confidence"] >= self.threshold
-            ) and self.dict_first[i]["classes"] == self.pred_x_it[i][
-                "classes"
-            ]:
-                selected.append(i)
-                classes_selected.append(self.dict_first[i]["classes"])
+                self.dict_first[instance]["confidence"] >= self.threshold
+                or values["confidence"] >= self.threshold
+            ) and self.dict_first[instance]["classes"] == values["classes"]:
+                selected.append(instance)
+                classes_selected.append(self.dict_first[instance]["classes"])
 
         return selected, classes_selected
 
-    def rule_3(self):
+    def rule_3(self) -> tuple:
         """
         regra responsável por verificar se as classes são diferentes E  as
         confianças preditas são maiores que o limiar
 
         Returns:
-        a lista correspondente pela condição
+            - A lista correspondente pela condição;
+            - A classe de cada elemento da lista.
         """
         selected = []
 
-        for i in self.pred_x_it:
+        for instance, values in self.pred_x_it.items():
             if (
-                self.dict_first[i]["classes"] != self.pred_x_it[i]["classes"]
-                and self.dict_first[i]["confidence"] >= self.threshold
-                and self.pred_x_it[i]["confidence"] >= self.threshold
+                self.dict_first[instance]["classes"] != values["classes"]
+                and self.dict_first[instance]["confidence"] >= self.threshold
+                and values["confidence"] >= self.threshold
             ):
-                selected.append(i)
+                selected.append(instance)
 
         return selected, self.remember(selected)
 
-    def rule_4(self):
+    def rule_4(self) -> tuple:
         """
         regra responsável por verificar se as classes são diferentes E uma das
         confianças preditas é maior que o limiar
 
         Returns:
-        a lista correspondente pela condição
+            - A lista correspondente pela condição;
+            - A classe de cada elemento da lista.
         """
         selected = []
 
-        for i in self.pred_x_it:
-            if self.dict_first[i]["classes"] != self.pred_x_it[i][
-                "classes"
-            ] and (
-                self.dict_first[i]["confidence"] >= self.threshold
-                or self.pred_x_it[i]["confidence"] >= self.threshold
+        for instance, values in self.pred_x_it.items():
+            if (
+                self.dict_first[instance]["classes"] != values["classes"]
+                and (
+                    self.dict_first[instance]["confidence"] >= self.threshold
+                    or values["confidence"] >= self.threshold
+                )
             ):
-                selected.append(i)
+                selected.append(instance)
 
         return selected, self.remember(selected)
 
@@ -259,17 +311,29 @@ class BaseFlexConC(SelfTrainingClassifier):
 
         return init_acc
 
-    def add_new_labeled(self, selected_full, selected, pred):
+    def add_new_labeled(
+        self,
+        selected_full: list,
+        selected: list,
+        local_acc: float,
+        init_acc: float,
+        max_proba: list[float],
+        pred: list,
+    ):
         """
         Função que retorna as intâncias rotuladas
 
         Args:
-            selected_full: lista com os indices das instâncias originais
-            selected: lista das intâncias com acc acima do limiar
-            local_acc: acurácia do modelo treinado com base na lista selected
-            init_acc: acurácia do modelo treinado com base na lista selected_full
-            max_proba: valores de probabilidade de predição das intâncias não rotuladas
-            pred: predição das instâncias não rotuladas
+            - selected_full: lista com os indices das instâncias
+                originais.
+            - selected: lista das intâncias com acc acima do limiar.
+            - local_acc: acurácia do modelo treinado com base na lista
+                selected.
+            - init_acc: acurácia do modelo treinado com base na lista
+                selected_full.
+            - max_proba: valores de probabilidade de predição das
+                intâncias não rotuladas.
+            - pred: predição das instâncias não rotuladas.
         """
         self.transduction_[selected_full] = pred[selected]
         self.labeled_iter_[selected_full] = self.n_iter_
@@ -287,7 +351,7 @@ class BaseFlexConC(SelfTrainingClassifier):
                 f" added {len(selected)} new labels."
             )
 
-    def select_instances_by_rules(self):
+    def select_instances_by_rules(self) -> tuple:
         """
         Função responsável por gerenciar todas as regras de inclusão do método
 
@@ -301,4 +365,5 @@ class BaseFlexConC(SelfTrainingClassifier):
 
             if selected:
                 return np.array(selected), pred
+
         return np.array([]), ""
